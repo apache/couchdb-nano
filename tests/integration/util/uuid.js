28cbdab913fbf6fcdcd3eab53a98a4ba6d87ca19
Glynn Bird <glynn.bird@gmail.com>
2018-07-03 14:15:59 +0100

    amend integration/util tests wrt Promise return values

--- tests/integration/util/uuid.js
+++ tests/integration/util/uuid.js
@@ -19,21 +19,39 @@ var nano = helpers.nano;
 var it = harness.it;
 
 it('should generate three uuids', function(assert) {
-  nano.uuids(3, function(error, data) {
+  var p = nano.uuids(3, function(error, data) {
     assert.equal(error, null, 'should generate uuids');
     assert.ok(data, 'got response');
     assert.ok(data.uuids, 'got uuids');
     assert.equal(data.uuids.length, 3, 'got 3');
     assert.end();
   });
+  assert.ok(helpers.isPromise(p), 'returns Promise')
+  p.then(function(data) {
+    assert.ok(true, 'Promise is resolved');
+    assert.ok(data, 'got response');
+    assert.ok(data.uuids, 'got uuids');
+    assert.equal(data.uuids.length, 3, 'got 3');
+  }).catch(function(error) {
+    assert.ok(true, 'Promise is rejected');
+  });
 });
 
 it('should generate one uuid', function(assert) {
-  nano.uuids(function(error, data) {
+  var p = nano.uuids(function(error, data) {
     assert.equal(error, null, 'should generate uuids');
     assert.ok(data, 'got response');
     assert.ok(data.uuids, 'got uuid');
     assert.equal(data.uuids.length, 1, 'got 1');
     assert.end();
+  });
+  assert.ok(helpers.isPromise(p), 'returns Promise')
+  p.then(function(data) {
+    assert.ok(true, 'Promise is resolved');
+    assert.ok(data, 'got response');
+    assert.ok(data.uuids, 'got uuid');
+    assert.equal(data.uuids.length, 1, 'got 1');
+  }).catch(function(error) {
+    assert.ok(true, 'Promise is rejected');
   });
 });
