cd83db07e14f35019a45ff5342af98dc47acda7b
Sam Smith <samsmith@linux.vnet.ibm.com>
2018-10-29 15:19:47 +0000

    Pass request agent to cloudant-follow

--- tests/helpers/unit.js
+++ tests/helpers/unit.js
@@ -89,6 +89,11 @@ helpers.unit = function (method, error) {
             stub.db = helpers.couch + stub.db
           }
 
+	      if (method[1] === 'follow' || method[1] === 'followUpdates') {
+            assert.ok(typeof req.httpAgent, 'object');
+            delete req.httpAgent; // ignore 'httpAgent' in deep equal assert
+          }
+
           assert.deepEqual(req, stub)
           assert.end()
         })
