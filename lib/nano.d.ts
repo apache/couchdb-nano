6a472c5313e51c95bf9d7812d72f6b262434aa8b
Samriddhi <singhsamriddhi5@gmail.com>
2019-04-23 13:30:14 -0700

    Update nano.d.ts
    Adding null to the MangoValue

--- lib/nano.d.ts
+++ lib/nano.d.ts
@@ -1172,7 +1172,7 @@ declare namespace nano {
     update_seq: any;
   }
 
-  type MangoValue = number | string | Date | boolean;
+  type MangoValue = number | string | Date | boolean | null;
 
   // http://docs.couchdb.org/en/latest/api/database/find.html#selector-syntax
   interface MangoSelector {
