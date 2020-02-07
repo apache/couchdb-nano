bb5428f0c532131675883c592e8b7c682149a10f
Glynn Bird <glynn.bird@gmail.com>
2020-02-07 14:09:57 +0000

    multipart tests

--- test/design.search.test.js
+++ test/design.search.test.js
@@ -15,7 +15,7 @@ const COUCH_URL = 'http://localhost:5984'
 const nano = Nano(COUCH_URL)
 const nock = require('nock')
 
-test('should be able to access a search index - GET /db/_design/ddoc/_search/searchname - db.search', async () => {
+test('should be able to access a search index - POST /db/_design/ddoc/_search/searchname - db.search', async () => {
   // mocks
   const response = {
     total_rows: 100000,
