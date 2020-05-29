58f59a97a8eac67b03e655e8c7a7f18507e0832d
Steven Tang <steven.yc.tang@gmail.com>
2020-05-29 20:57:11 +1000

    fix(nano.d.ts): add opts and callback for nano.request

--- lib/nano.d.ts
+++ lib/nano.d.ts
@@ -33,7 +33,7 @@ declare namespace nano {
     db: DatabaseScope;
     use<D>(db: string): DocumentScope<D>;
     scope<D>(db: string): DocumentScope<D>;
-    request: Promise<any>;
+    request(opts: RequestOptions | string, callback?: Callback<any>): Promise<any>;
     relax: Promise<any>;
     dinosaur: Promise<any>;
     // http://docs.couchdb.org/en/latest/api/server/authn.html#cookie-authentication
@@ -412,11 +412,6 @@ declare namespace nano {
     url: string;
     db: string;
   }
-
-  type RequestFunction = (
-    options?: RequestOptions | string,
-    callback?: Callback<any>
-  ) => void;
 
   interface RequestOptions {
     db?: string;
