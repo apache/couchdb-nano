4155cd637920315bf59fa6aa13d53b34387c833e
Glynn Bird <glynnbird@apache.org>
2023-01-26 14:26:01 +0000

    old assert for node14

--- test/cookie.test.js
+++ test/cookie.test.js
@@ -10,7 +10,7 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
-const assert = require('node:assert/strict')
+const assert = require('assert')
 
 const CookieJar = require('../lib/cookie.js')
 
